88a572078ca471a6c3a043d4cfafa0cd151d2d62
rain_shine <lingerwan123@GMAIL.COM>
2022-08-15 15:55:33 +1000

    Sync

--- src/avow.rs
+++ src/avow.rs
@@ -12,6 +12,9 @@ use curve25519_dalek::constants::{RISTRETTO_BASEPOINT_TABLE};
 use curve25519_dalek::ristretto::{RistrettoPoint, CompressedRistretto};
 use curve25519_dalek::scalar::{Scalar};
 use rand::rngs::OsRng;
+
+use tokio::{runtime, time};
+ 
 #[allow(non_camel_case_types,dead_code,non_snake_case)]
 #[derive(Debug)]
 pub struct avow_proof{
@@ -26,7 +29,7 @@ use futures::{StreamExt, SinkExt, TryStreamExt};
 /// 
 /// 
 #[allow(unused_variables,non_snake_case,unused_mut)]
-pub async fn avow(alice:PublicKey, bob: PublicKey, judge: PublicKey, sk: StaticSecret, secret_a: StaticSecret, secret_b:StaticSecret, role:bool){
+pub async fn avow(alice:PublicKey, bob: PublicKey, judge: PublicKey, sk_a: StaticSecret, sk_b: StaticSecret, secret_a: StaticSecret, secret_b:StaticSecret, role:bool, k_session:[u8;32]){
 
 
         let c_A = StaticSecret::new(&mut OsRng);
@@ -56,7 +59,9 @@ pub async fn avow(alice:PublicKey, bob: PublicKey, judge: PublicKey, sk: StaticS
         //Recv E_A, R_A
         let RecvE_A = CompressedRistretto(r12.try_next().await.unwrap().unwrap().freeze().to_vec().try_into().unwrap()).decompress().unwrap();
         let RecvR_A = CompressedRistretto(r12.try_next().await.unwrap().unwrap().freeze().to_vec().try_into().unwrap()).decompress().unwrap();
-
+        
+        assert_eq!(RecvE_A,E_A);
+        assert_eq!(RecvR_A,R_A);
 
 
         let c_B = StaticSecret::new(&mut OsRng);
@@ -64,9 +69,29 @@ pub async fn avow(alice:PublicKey, bob: PublicKey, judge: PublicKey, sk: StaticS
         let r_B = StaticSecret::new(&mut OsRng);
         let R_B = &r_B.0 * & RISTRETTO_BASEPOINT_TABLE;
     
-        let cipher = Aes256Gcm::new(GenericArray::from_slice( &Scalar::random(&mut OsRng).to_bytes()));
+        let cipher = Aes256Gcm::new(GenericArray::from_slice( &k_session));
         let nonce = Nonce::from_slice(b"avow_key_exc"); // 96-bits; unique per message
-        let ciphertext = cipher.encrypt(nonce, c_A.to_bytes().iter().chain(&z_A.to_bytes()).chain(&s_A.to_bytes()).cloned().collect::<Vec<_>>().as_ref());
+        let ciphertext = cipher.encrypt(nonce, c_A.to_bytes().iter()
+                                                                                .chain(&z_A.to_bytes())
+                                                                                .chain(&s_A.to_bytes())
+                                                                                .cloned().collect::<Vec<_>>().as_ref()).unwrap();
+        
+        //send ciphertext and R_B
+        s21.send(Bytes::copy_from_slice(&R_B.compress().to_bytes())).await.unwrap();
+        s21.send(Bytes::from(ciphertext)).await.unwrap();
+
+        //Alice gets R_B and the ciphertext, decrypts it to get plaintext
+        let Recv_R_B = r21.try_next().await.unwrap().unwrap().freeze();
+        
+        let Recv_ciphertext = r21.try_next().await.unwrap().unwrap().freeze();
+        let Recv_plaintext = cipher.decrypt(nonce, Recv_ciphertext.to_vec().as_slice()).unwrap();
+        let Recv_c_A:StaticSecret = StaticSecret(Scalar::from_bits( Recv_plaintext[..32].try_into().unwrap()));
+        let Recv_z_A:StaticSecret = StaticSecret(Scalar::from_bits( Recv_plaintext[32..64].try_into().unwrap())); 
+        let Recv_s_A:StaticSecret = StaticSecret(Scalar::from_bits( Recv_plaintext[64..96].try_into().unwrap()));
+
+        assert_eq!(Recv_c_A.0 , c_A.0);
+        assert_eq!(Recv_z_A.0 , z_A.0);
+        assert_eq!(Recv_s_A.0 , s_A.0);
         let c_AB = prove_avow(c_A, c_B, z_A, z_B, R_A, R_B, judge);
 
         let z_alpha = c_AB.0 * secret_a.0 + r_A.0;
@@ -90,4 +115,12 @@ fn prove_avow(c_A:StaticSecret,c_B:StaticSecret,z_A:StaticSecret,z_B:StaticSecre
 
 pub fn Judge(pk_J: PublicKey, AB: RistrettoPoint, pi: avow_proof ) -> bool{
     true
+}
+
+#[test]
+fn test_avow(){
+    let mut rt = runtime::Builder::new().basic_scheduler().enable_all().build().unwrap();
+    rt.block_on(async move{
+    avow().await;
+    })
 }
